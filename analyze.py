b20b7779ecb27718d8d4395eaf889d0b654f08e9
TRAN THANH SON <91709267+FOX2920@users.noreply.github.com>
2024-10-26 13:37:18 +0700

    Update analyze.py

--- analyze.py
+++ analyze.py
@@ -43,6 +43,7 @@ def dashboard():
 
     if uploaded_file is not None:
         df = pd.read_csv(uploaded_file)
+        df =df.drop_duplicates()
         df.rename(columns={
                 'Điểm tổng quát soft skill' : 'Điểm tổng quát theo tiêu chí', 
                 'Điểm tổng quát hard skill':'Điểm tổng quát theo CV', 
